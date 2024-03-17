8655a19afc503168cae354db112b3799a5fcab5d
rwaming <art.rwam@gmail.com>
2024-03-17 21:56:23 +0900

    Feat: Get lastest video and publish date of that to use gapi
    But an error occures
    ./app/page.tsx
    Error:
      × You're importing a component that needs useEffect. It only works in a Client Component but none of its parents are marked with "use client", so they're Server Components by default.
      │ Learn more: https://nextjs.org/docs/getting-started/react-essentials
      │
      │
       ╭─[/Users/rwam/project/next-karaoke-remote/app/page.tsx:1:1]
     1 │ import { Suspense, useEffect, useState } from 'react'
       ·                    ─────────
     2 │ import Script from 'next/script'
     3 │
     4 │ export default function Home() {
       ╰────
    
      × You're importing a component that needs useState. It only works in a Client Component but none of its parents are marked with "use client", so they're Server Components by default.
      │ Learn more: https://nextjs.org/docs/getting-started/react-essentials
      │
      │
       ╭─[/Users/rwam/project/next-karaoke-remote/app/page.tsx:1:1]
     1 │ import { Suspense, useEffect, useState } from 'react'
       ·                               ────────
     2 │ import Script from 'next/script'
     3 │
     4 │ export default function Home() {
       ╰────

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,53 @@
-import { Suspense } from 'react'
+import { Suspense, useEffect, useState } from 'react'
+import Script from 'next/script'
 
 export default function Home() {
+  const [videoID, setVidoeID] = useState('')
+  const [video, setVidoe] = useState('')
+  const [videoDate, setVidoeDate] = useState('')
+
+  // Load latest video
+  useEffect(() => {
+    gapi.client
+      .init({
+        apiKey: 'AIzaSyB1IOFOJ0D_e2-16KS4Tlol7mAiN2x9Fl4',
+        discoveryDocs: [
+          'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest',
+        ],
+        clientId: 'art.rwam.apps.googleusercontent.com',
+        scope: 'profile',
+      })
+      .then(() => {
+        //@ts-ignore
+        return gapi.client.youtube.search
+          .list({
+            part: 'snippet',
+            channelId: 'UCDqaUIUSJP5EVMEI178Zfag',
+            order: 'date',
+          })
+          .then((response: any) => {
+            const latestVideo = response.result.items[0]
+            const latestDate = latestVideo.publishTime
+            setVidoe(latestVideo)
+            setVidoeDate(latestDate)
+          })
+      })
+  }, [])
+
   return (
-    <div className="wrap">
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className="media"></div>
-        <div className="control">
-          <button type="button" className="pause bg-orange-300">
-            ⏯️
-          </button>
-        </div>
-      </Suspense>
-    </div>
+    <>
+      <Script src="https://apis.google.com/js/api.js" />
+
+      <div className="wrap">
+        <Suspense fallback={<div>Loading...</div>}>
+          <div className="vedio"></div>
+          <div className="control">
+            <button type="button" className="pause bg-orange-300">
+              ⏯️
+            </button>
+          </div>
+        </Suspense>
+      </div>
+    </>
   )
 }
