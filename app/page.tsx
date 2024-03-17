836bf633d770733054655ab3ba28bc7b1e38c7ad
rwaming <art.rwam@gmail.com>
2024-03-17 17:01:50 +0900

    Feat: Rough basic markup for pause

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-import Image from 'next/image'
+import { Suspense } from 'react'
 
 export default function Home() {
-  return <div></div>
+  return (
+    <div className="wrap">
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="media"></div>
+        <div className="control">
+          <button type="button" className="pause bg-orange-300">
+            ⏯️
+          </button>
+        </div>
+      </Suspense>
+    </div>
+  )
 }
