62a6a756e791bced478356c087ae8692ed35dd79
rwaming <art.rwam@gmail.com>
2024-03-18 00:41:54 +0900

    Chore: Make the function not run for a time

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,52 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Script from 'next/script'
 import { gapi } from 'gapi-script'
 
 export default function Home() {
-  const [video, setVideo] = useState('idle')
-  const [videoID, setVideoID] = useState('idle')
-  const [videoTitle, setVideoTitle] = useState('idle')
-  const [videoDate, setVideoDate] = useState('idle')
+  const [video, setVideo] = useState('video')
+  const [videoID, setVideoID] = useState('videoID')
+  const [videoTitle, setVideoTitle] = useState('videoTitle')
+  const [videoDate, setVideoDate] = useState('videoDate')
 
   // Load latest video
-  useEffect(() => {
-    async function youtubeApiFirst() {
-      gapi.load('client', () => {
-        gapi.client
-          .init({
-            apiKey: 'AIzaSyB1IOFOJ0D_e2-16KS4Tlol7mAiN2x9Fl4',
-            discoveryDocs: [
-              'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest',
-            ],
-            clientId:
-              '73717891696-055at71c0fqi44m975h68s1ktgiqrqob.apps.googleusercontent.com',
-            scope: 'profile',
-          })
-          .then(() => {})
-          .then(() => {
-            //@ts-ignore
-            return gapi.client.youtube.search
-              .list({
-                part: 'snippet',
-                channelId: 'UCDqaUIUSJP5EVMEI178Zfag',
-                order: 'date',
-              })
-              .then((response: any) => {
-                const latestVideoInfo = response.result.items[0]
-                const latestVideoID = latestVideoInfo.id.videoId
-                const latestVideoTitle = latestVideoInfo.snippet.description
-                const latestVideoDate = latestVideoInfo.snippet.publishedAt
-                setVideoID(latestVideoID)
-                setVideoTitle(latestVideoTitle)
-                setVideoDate(latestVideoDate)
-              })
-          })
-      })
-    }
-    youtubeApiFirst()
-  }, [])
+  const firstVideoRef = useRef(async function youtubeApiFirst() {
+    console.log('start to load')
+    /* It should be used in really needed!!
+    It resets when 4pm in Korea.
+    gapi.load('client', () => {
+      gapi.client
+        .init({
+          apiKey: 'AIzaSyB1IOFOJ0D_e2-16KS4Tlol7mAiN2x9Fl4',
+          discoveryDocs: [
+            'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest',
+          ],
+          clientId:
+            '73717891696-055at71c0fqi44m975h68s1ktgiqrqob.apps.googleusercontent.com',
+          scope: 'profile',
+        })
+        .then(() => {})
+        .then(() => {
+          //@ts-ignore
+          return gapi.client.youtube.search
+            .list({
+              part: 'snippet',
+              channelId: 'UCDqaUIUSJP5EVMEI178Zfag',
+              order: 'date',
+            })
+            .then((response: any) => {
+              const latestVideoInfo = response.result.items[0]
+              const latestVideoID = latestVideoInfo.id.videoId
+              const latestVideoTitle = latestVideoInfo.snippet.description
+              const latestVideoDate = latestVideoInfo.snippet.publishedAt
+              setVideoID(latestVideoID)
+              setVideoTitle(latestVideoTitle)
+              setVideoDate(latestVideoDate)
+            })
+        })
+    })*/
+  })
 
   return (
     <>
@@ -54,12 +54,22 @@ export default function Home() {
 
       <div className="wrap">
         <div className="vedio">
-          <p>{videoID}</p>
-          <p>{videoTitle}</p>
+          <p className="text-emerald-800">{videoID}</p>
+          <p className="text-slate-600">{videoTitle}</p>
           <p>{videoDate}</p>
+          <button
+            type="button"
+            className="border border-orange-200 bg-red-400 rounded-md p-1"
+            onClick={() => firstVideoRef.current()}
+          >
+            Latest Song
+          </button>
         </div>
         <div className="control">
-          <button type="button" className="pause bg-orange-300">
+          <button
+            type="button"
+            className="pause border border-green-800 bg-blue-900 rounded-md p-1 px-2"
+          >
             ⏯️
           </button>
         </div>
