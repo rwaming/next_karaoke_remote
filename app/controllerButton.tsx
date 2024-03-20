74243b6f514129671b9a9b9dbba16880212b700c
rwaming <art.rwam@gmail.com>
2024-03-21 01:03:34 +0900

    Move: Make applauseAudio function a new tsx file

--- app/controllerButton.tsx
+++ app/controllerButton.tsx
@@ -1,10 +1,4 @@
-import {
-  useCallback,
-  useContext,
-  useMemo,
-  useRef,
-  type MouseEvent,
-} from 'react'
+import { useCallback, useContext, useRef, type MouseEvent } from 'react'
 import AppContext from './AppContext'
 import {
   applause,
@@ -15,6 +9,7 @@ import {
   showLatestVideo,
   stop,
 } from './controllerFunctions'
+import ApplauseAudios from './applauseAudios'
 
 export default function ControllerButton({
   id,
@@ -62,131 +57,6 @@ export default function ControllerButton({
     ],
   )
 
-  const applauseAudios = useMemo(
-    () =>
-      [...Array(4)].map((e, i) => {
-        if (i === 0) {
-          return (
-            <audio
-              ref={applauseRef1}
-              key={`${id}__audio-${i + 1}`}
-              id={`${id}__audio-${i + 1}`}
-              preload="auto"
-            >
-              <source src="/applause.mp3" type="audio/mpeg" />
-              <track
-                src="/applause_en.vtt"
-                kind="captions"
-                srcLang="en"
-                label="English"
-              />
-              <track
-                src="/applause_ko.vtt"
-                kind="captions"
-                srcLang="ko"
-                label="Korean"
-              />
-            </audio>
-          )
-        }
-        if (i === 1) {
-          return (
-            <audio
-              ref={applauseRef2}
-              key={`${id}__audio-${i + 1}`}
-              id={`${id}__audio-${i + 1}`}
-              preload="auto"
-            >
-              <source src="/applause.mp3" type="audio/mpeg" />
-              <track
-                src="/applause_en.vtt"
-                kind="captions"
-                srcLang="en"
-                label="English"
-              />
-              <track
-                src="/applause_ko.vtt"
-                kind="captions"
-                srcLang="ko"
-                label="Korean"
-              />
-            </audio>
-          )
-        }
-        if (i === 2) {
-          return (
-            <audio
-              ref={applauseRef3}
-              key={`${id}__audio-${i + 1}`}
-              id={`${id}__audio-${i + 1}`}
-              preload="auto"
-            >
-              <source src="/applause.mp3" type="audio/mpeg" />
-              <track
-                src="/applause_en.vtt"
-                kind="captions"
-                srcLang="en"
-                label="English"
-              />
-              <track
-                src="/applause_ko.vtt"
-                kind="captions"
-                srcLang="ko"
-                label="Korean"
-              />
-            </audio>
-          )
-        }
-        return (
-          <audio
-            ref={applauseRef4}
-            key={`${id}__audio-${i + 1}`}
-            id={`${id}__audio-${i + 1}`}
-            preload="auto"
-          >
-            <source src="/applause.mp3" type="audio/mpeg" />
-            <track
-              src="/applause_en.vtt"
-              kind="captions"
-              srcLang="en"
-              label="English"
-            />
-            <track
-              src="/applause_ko.vtt"
-              kind="captions"
-              srcLang="ko"
-              label="Korean"
-            />
-          </audio>
-        )
-      }),
-    [id],
-  )
-
-  // () =>
-  //   [...Array(4)].map((_, index) => (
-  //     <audio
-  //       ref={applauseRef}
-  //       key={`${id}__audio-${index + 1}`}
-  //       id={`${id}__audio-${index + 1}`}
-  //       preload="auto"
-  //     >
-  //       <source src="/applause.mp3" type="audio/mpeg" />
-  //       <track
-  //         src="/applause_en.vtt"
-  //         kind="captions"
-  //         srcLang="en"
-  //         label="English"
-  //       />
-  //       <track
-  //         src="/applause_ko.vtt"
-  //         kind="captions"
-  //         srcLang="ko"
-  //         label="Korean"
-  //       />
-  //     </audio>
-  //   )),
-
   return (
     <>
       <button
@@ -200,7 +70,12 @@ export default function ControllerButton({
         {text}
       </button>
       {id.includes('applause') && (
-        <div id={`${id}__audio-list`}>{applauseAudios}</div>
+        <div id={`${id}__audio-list`}>
+          <ApplauseAudios
+            audioRefs={[applauseRef1, applauseRef2, applauseRef3, applauseRef4]}
+            id={id}
+          />
+        </div>
       )}
     </>
   )
